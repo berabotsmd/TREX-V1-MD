4e19205c893c89b25192f4b0e22b18f30214a2e9
berabotsmd <berahiggins@gmail.com>
2024-08-22 15:21:54 +0300

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Property Of @FXastro
+
+FROM debian:bullseye-slim
+
+ENV NODE_VERSION 20.16.0
+
+RUN apt-get update && \
+    apt-get install -y \
+    curl \
+    git \
+    ffmpeg \
+    webp && \
+    apt-get upgrade -y && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN curl -fsSL https://deb.nodesource.com/setup_18.x | bash - && \
+    apt-get install -y nodejs
+
+RUN npm install -g yarn
+
+RUN git clone https://github.com/FXastro/fxop-md.git /root/bot
+WORKDIR /root/bot
+RUN npm install
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const config = require('./config')
+const connect = require('./lib/client')
+const io = require('socket.io-client')
+const { getandRequirePlugins } = require('./lib/database/plugins')
+const { fetchFiles, createSession } = require('./lib/Misc')
+const { delay } = require('baileys')
+const RunServer = require('./server')
+async function startBot() {
+ try {
+  const server = new RunServer(3000)
+  server.start()
+  delay(3000)
+  await createSession()
+  await fetchFiles(path.join(__dirname, '/lib/database/'))
+  console.log('Syncing Database')
+  await config.DATABASE.sync()
+  console.log('⬇  Installing Plugins...')
+  await fetchFiles(path.join(__dirname, '/plugins/'))
+  await getandRequirePlugins()
+  console.log('Plugins Installed!')
+
+  const ws = io('https://socket-counter.onrender.com/', { reconnection: true })
+  ws.on('connect', () => console.log('Connected to server'))
+  ws.on('disconnect', () => console.log('Disconnected from server'))
+
+  return await connect()
+ } catch (error) {
+  console.error('Initialization error:', error)
+  return process.exit(1) // Exit with error status
+ }
+}
+
+startBot()
